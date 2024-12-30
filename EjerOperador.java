c4c6c4fab46ea1d7b770c715e49fc015d452833a
Jesús Chávez Arias <l19141132@queretaro.tecnm.mx>
2024-12-30 15:42:58 -0600

    Ejer1Operadores

---
+++ EjerOperador.java
@@ -0,0 +1,13 @@
+/*
+    Descripción:
+    Escribe un programa que lea tres números enteros del usuario y determine si:
+
+    Al menos uno de los números es positivo.
+    Todos los números son negativos.
+    Solo uno de los números es par.
+ */
+public class EjerOperador {
+    public static void main(String[] args) {
+        
+    }
+}
