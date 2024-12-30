edef7d63296754fb4885962f45ea37e84d1117f2
Jesús Chávez Arias <l19141132@queretaro.tecnm.mx>
2024-12-30 14:19:56 -0600

    Operaciones basicas

--- Operadores.java
+++ Operadores.java
@@ -1,5 +1,10 @@
 public class Operadores {
     public static void main(String[] JCA){
-
+        //Operadores Básicos: +, -, *, /, %
+        int a = 100, b = 50;
+        System.out.println("Suma de "+a+" y "+b+" Es: "+(a+b));
+        System.out.println("Resta de "+a+" y "+b+" Es: "+(a-b));
+        System.out.println("La multiplicacón de "+a+" y "+b+" Es:"+(a+b));
+        System.out.println("La división de "+a+" y "+b+" Es: "+(a/b));
     }
 }
