3bbac4c41eff128a79d135c793261a00785fcc43
Jesús Chávez Arias <l19141132@queretaro.tecnm.mx>
2024-12-30 15:30:15 -0600

    Operadores Lógicos and instanceof

--- Operadores.java
+++ Operadores.java
@@ -6,5 +6,24 @@ public class Operadores {
         System.out.println("Resta de "+a+" y "+b+" Es: "+(a-b));
         System.out.println("La multiplicacón de "+a+" y "+b+" Es:"+(a+b));
         System.out.println("La división de "+a+" y "+b+" Es: "+(a/b));
+
+
+         // Operadores Lógicos: &&, ||, !
+         boolean x = true, y = false;
+         System.out.println("AND LOGICO: "+(x&&y));
+         System.out.println("OR LOGICO: "+(x||y));
+         System.out.println("NOT LOGICO: "+!(!x));
+
+         operador();
+    }
+    public static void operador(){
+        Object obj = "Hola, Java 17!";
+        
+        // Usando instanceof mejorado
+        if (obj instanceof String s) {
+            System.out.println("Longitud de la cadena: " + s.length());
+        } else {
+            System.out.println("El objeto no es una cadena.");
+        }
     }
 }
