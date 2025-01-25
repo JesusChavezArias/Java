4deb51c9647d3efaae6b86f37fba8032e22b8999
Jesús Chávez Arias <l19141132@queretaro.tecnm.mx>
2025-01-25 15:11:00 -0600

    PowerofTwoV2
    Approach 2(Dividing n by 2)
    
        If n is divisible by 2 , every time we will encounter 1 at the end.
        And at any point if n isn't divisible by 2 we will return false.
    
    Complexity
    
        Time complexity:
        O(logn)
    
        Space complexity:
        O(1)

---
+++ LeetCode/PowerofTwoV2.java
@@ -0,0 +1,51 @@
+/*
+Given an integer n, return true if it is a power of two. Otherwise, return false.
+
+An integer n is a power of two, if there exists an integer x such that n == 2x.
+
+ 
+
+Example 1:
+
+Input: n = 1
+Output: true
+Explanation: 20 = 1
+
+Example 2:
+
+Input: n = 16
+Output: true
+Explanation: 24 = 16
+
+Example 3:
+
+Input: n = 3
+Output: false
+
+ 
+
+Constraints:
+
+    -231 <= n <= 231 - 1
+ */
+
+public class PowerofTwoV2 {
+    public static void main(String[] args) {
+        int n = 536870912;
+        Solution solution = new Solution();
+        boolean resultado = solution.isPowerOfTwo(n);
+        System.out.println(resultado);
+    }
+}
+
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+        for (int i = 0; i < 31; i++) {
+            int ans = (int) Math.pow(2, i);
+            if (ans == n) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
